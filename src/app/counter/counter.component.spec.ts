016111ccee29e5991d562187341cdfb4743eb4aa
Lei ZHU <lzhu19@slb.com>
2020-11-27 15:31:08 +0800

    feat: implement increase count function

---
+++ src/app/counter/counter.component.spec.ts
@@ -0,0 +1,34 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CounterComponent } from './counter.component';
+
+describe('CounterComponent', () => {
+  let component: CounterComponent;
+  let fixture: ComponentFixture<CounterComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CounterComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CounterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should create count when click add button', () => {
+    //given
+    component.count = 0;
+    //when
+    component.increaseCount();
+    //then
+    expect(component.count).toBe(1);
+  });
+});
---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.css']
+})
+export class CounterComponent implements OnInit {
+
+  count: number;
+
+  constructor() {
+    this.count = 0;
+  }
+
+  ngOnInit(): void {
+  }
+
+  increaseCount(): void {
+    this.count++;
+  }
+
+}
